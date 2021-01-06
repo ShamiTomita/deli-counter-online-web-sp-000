93179661fbe3628a51cf1ee68eca16988de24d1d
Shami Tomita <shamitomita@gmail.com>
2021-01-06 21:38:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,8 +26,7 @@ def now_serving(katz_deli)
   if katz_deli.length <= 1 
   puts "There is nobody waiting to be served!"
   else 
-    x = "Currently serving #{(katz_deli[0])}."
+    puts "Currently serving #{(katz_deli[0])}."
     katz_deli.delete_at(0)
-    return x 
   end
 end 
